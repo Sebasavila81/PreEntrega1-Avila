3f2cd2e2cdd1664d953ec9b1e0f999145c47d829
Sebastian Avila <sebasavila81@gmail.com>
2023-05-14 20:18:18 -0300

    practicas

--- ej_comp/ej_comp.js
+++ ej_comp/ej_comp.js
@@ -356,3 +356,28 @@ for (let index = 0; index < 5; index++) {
         "rubro: " + tienda.rubro + "\n";
 }
 alert(ingresados); */
+
+/* Actividad 2*/
+/* Solicitar al usuario el registro de cinco (5) tiendas.
+Emplear la clase Tienda de la actividad 1, para instanciar los objetos en función de las entradas capturadas. Generar una única salida compuesta por la información de los objetos instanciados.
+Es posible instanciar objetos de forma local en un bloque, y los valores pasados por parámetro al constructor pueden ser capturados desde una llamada prompt. */
+
+class Tienda {
+    constructor(nombre, direccion, propietario, rubro) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.propietario = propietario;
+        this.rubro = rubro;
+
+    }
+}
+let ingresados = ""
+for(i = 0 ; i < 5 ; i++){
+    let tienda1 = new Tienda (prompt("Ingrese nombre"), 
+                                ("Ingrese direccion"),
+                                ("Ingrese propietario"), 
+                                ("Ingrese Rubro"));
+
+    ingresados += 
+
+}
